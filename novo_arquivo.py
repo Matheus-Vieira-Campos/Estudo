784eb4acf2c0bd77615f9f84acc650886ddcbde2
Matheus Vieira <matheusvcampos84@gmail.com>
2026-07-20 13:43:22 -0300

    adciona um novo exercicio

--- novo_arquivo.py
+++ novo_arquivo.py
@@ -1,1 +1,15 @@
-print("MatheusDev")
+# Exercício.
+# Faça um programa que percorra a palavra "Matheus" e coloque um * antes e depois de cada letra.
+# Resultado esperado: *M**a**t**h**e**u**s*
+
+nome = "Matheus"
+indice = 0 
+novo_nome = ""
+
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f"**{letra}"
+    indice += 1
+
+novo_nome += "*"
+print(novo_nome)
